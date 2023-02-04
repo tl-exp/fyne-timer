7dbe5baa30ebe08139675658a54bebaf3cc7bb1f
tl-exp <>
2023-02-05 00:14:55 +0200

    Timer first build

---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"time"
+	"gopkg.in/yaml.v3"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/systray"
+)
+
+
+type Subject struct {
+    Title string `yaml:"title"`
+    Duration time.Duration `yaml:"duration"`
+    Author string `yaml:"author"`
+}
+
+type Schedule struct {
+    Subjects []Subject `yaml:"subjects"`
+    Version string `yaml:"version"`
+}
+
+
+func ReadConfig(filename string) (*Schedule, error) {
+    buf, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+
+    schedule := &Schedule{}
+    err = yaml.Unmarshal(buf, schedule)
+    if err != nil {
+        return nil, fmt.Errorf("in file %q: %w", filename, err)
+    }
+    if schedule.Version != "1"  {
+    	return nil, fmt.Errorf("in file %q: Unsupported version %s", filename, schedule.Version)
+    }
+
+    return schedule, err
+}
+
+
+func fmtDuration(d time.Duration) string {
+	sign := ""
+	if d < 0 {
+		sign = "-"
+		d = -d
+	}
+    d = d.Round(time.Second)
+    h := d / time.Hour
+    d -= h * time.Hour
+    m := d / time.Minute
+    d -= m * time.Minute
+    s := d / time.Second
+    return fmt.Sprintf("%s%02d:%02d:%02d", sign, h, m, s)
+}
+
+
+const AppName = "Meeting Timer"
+
+func main() {
+	schedule, err := ReadConfig("./schedule.yaml")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	a := app.New()
+	w := a.NewWindow(AppName)
+	trayUpdater := Tray{}
+
+	runner := NewScheduleRunner(schedule)
+
+	mainWindow := MainWindow{w, runner}
+
+	runner.SetCallbackOnTick(trayUpdater.Update)
+	runner.SetCallbackOnSubjectFinish(func (s *SubjectRunner) {mainWindow.UpdateAndShow()})
+
+	if desk, ok := a.(desktop.App); ok {
+		m := fyne.NewMenu(AppName,
+			fyne.NewMenuItem("Show", func () {mainWindow.UpdateAndShow()}))
+		desk.SetSystemTrayMenu(m)
+	}
+
+	w.SetCloseIntercept(func() {
+		w.Hide()
+	})
+
+
+	runner.Start()
+	defer runner.Stop()
+	a.Run()
+}
+
+// just to not update too often
+type Tray struct {
+	LastIcon string
+	LastTitle string
+}
+
+
+func (t *Tray) Update(s *SubjectRunner) {
+	duration := s.Subject.Duration - s.RunTime
+	rest := fmtDuration(duration)
+	var resource *fyne.StaticResource
+
+	if duration > 0 {
+		resource = resourceGreenIco
+	} else {
+		resource = resourceRedIco
+	}
+
+	title := fmt.Sprintf("%s: %s", s.Subject.Title, rest)
+	if t.LastTitle != title {
+		systray.SetTitle(title)
+		systray.SetTooltip(title)
+		t.LastTitle = title
+	}
+
+	if t.LastIcon != resource.StaticName {
+		t.LastIcon = resource.StaticName
+		systray.SetIcon(resource.StaticContent)
+	}
+}
---
+++ main_window.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"image/color"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/container"
+
+)
+
+type MainWindow struct {
+	window fyne.Window
+	runner *ScheduleRunner
+}
+
+
+func (m *MainWindow) UpdateAndShow() {
+	content := m.BuildMainWindow()
+	m.window.SetContent(content)
+	m.window.Show()
+}
+
+
+func (m *MainWindow) BuildMainWindow() fyne.CanvasObject {
+
+	var StatusColors = map[SubjectStatus]color.Color{
+	  Ready: color.Black,
+	  Running:  color.RGBA{0, 200, 0, 255},
+	  Done: color.RGBA{0, 0, 255, 255},
+	  Skipped: color.RGBA{82, 82, 82, 255},
+	}
+
+	var ButtonText = map[SubjectStatus]string{
+	  Ready: "Start",
+	  Running:  "Continue",
+	  Done: "Start Again",
+	  Skipped: "",
+	}
+
+
+	var subjects []fyne.CanvasObject
+	subjects = append(subjects,
+		widget.NewLabel(""),
+		widget.NewLabelWithStyle("Title", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle("Author", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+		widget.NewLabelWithStyle("Duration", fyne.TextAlignCenter, fyne.TextStyle{Bold: true}),
+	)
+	for i := 0; i < len(m.runner.Subjects); i++ {
+		i := i
+		subj := m.runner.Subjects[i]
+		text := canvas.NewText(subj.Subject.Title, StatusColors[subj.Status])
+		text.Alignment = fyne.TextAlignCenter
+		text.TextStyle = fyne.TextStyle{Monospace: true}
+
+		subjects = append(subjects,
+			widget.NewButton(ButtonText[subj.Status], func() { 
+				m.runner.StartSubject(i) 
+				m.window.Hide()
+			}),
+			text,
+			widget.NewLabelWithStyle(subj.Subject.Author, fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+			widget.NewLabelWithStyle(fmtDuration(subj.Subject.Duration), fyne.TextAlignCenter, fyne.TextStyle{Monospace: true}),
+		)
+	}
+	content := container.New(layout.NewGridLayout(4), subjects...)
+
+	return content
+}
